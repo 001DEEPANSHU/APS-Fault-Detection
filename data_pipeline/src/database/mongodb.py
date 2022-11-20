cf7883ba213d2b66e040d629fb4ca835880c1f54
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-20 18:08:15 +0530

    fixed mongo url env variable

--- data_pipeline/src/database/mongodb.py
+++ data_pipeline/src/database/mongodb.py
@@ -6,11 +6,11 @@ ca = certifi.where()
 class MongodbOperation:
 
     def __init__(self) -> None:
-        self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'), tlsCAFile=ca)
+        self.client = pymongo.MongoClient(os.getenv('MONGODB_URL_KEY',None)) #MONGODB_URL_KEY
         self.db_name="APS_DB"
-
+        
     def insert_many(self,collection_name,records:list):
-        self.client[self.db_name][collection_name].insert_many(records)  #insert many records
-    
-    def insert_many(self,collection_name,record):
-        self.client[self.db_name][collection_name].insert_one(record)    #insert one record at a time
+        self.client[self.db_name][collection_name].insert_many(records)
+
+    def insert(self,collection_name,record):
+        self.client[self.db_name][collection_name].insert_one(record)
