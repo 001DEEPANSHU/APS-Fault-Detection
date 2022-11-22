5308afe42b6c870f94921b9c8533292647d5dd19
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-22 21:04:06 +0530

    Stage 3: Data Transformation related configs

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -13,3 +13,11 @@ class DataValidationArtifact:   # For Stage 2: Data Validation
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+
+@dataclass
+class DataTransformationArtifact:   # For Stage 3: Data Transformation
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
