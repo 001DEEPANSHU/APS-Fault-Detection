ecc7424c743517f653a49321ff85affe5a984173
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-22 19:26:48 +0530

    set data validation related entities

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,6 +1,15 @@
 from dataclasses import dataclass
 
 @dataclass
-class DataIngestionArtifact:
+class DataIngestionArtifact:    # For Stage 1: Data Ingestion
     trained_file_path: str
     test_file_path: str
+
+@dataclass
+class DataValidationArtifact:   # For Stage 2: Data Validation
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
