8d297dc61201f1c8818d30d09020a6c54a82e147
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-23 16:38:17 +0530

    Model trainer code

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -1,5 +1,6 @@
 import os
-from sensor.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+
+from sensor.constant.training_pipeline import MODEL_FILE_NAME, SAVED_MODEL_DIR
 
 
 class TargetValueMapping:
@@ -13,3 +14,26 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self.to_dict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+
+
+
+
+
+class SensorModel:
+
+    def __init__(self,preprocessor,model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise e
+    
+    def predict(self,x):    # Making Predictions
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise e
+    
+
