acd4f22dc153debe2b7f4fef614c9266943e8dcd
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-24 14:45:10 +0530

    Added Evaluation code to training pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,5 +1,5 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact, ModelEvaluationArtifact
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
@@ -7,6 +7,7 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
+from sensor.components.model_evaluation import ModelEvaluation
 
 
 class TrainPipeline:
@@ -15,6 +16,8 @@ class TrainPipeline:
         self.training_pipeline_config = TrainingPipelineConfig()
 
         
+
+
     # Stage 1: Data Ingestion
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
@@ -28,6 +31,7 @@ class TrainPipeline:
             raise  SensorException(e,sys)
 
 
+
     # Stage 2: Data Validation
     def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
         try:
@@ -38,6 +42,8 @@ class TrainPipeline:
             return data_validation_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
+
+
 
     # Stage 3: Data Transformation
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
@@ -63,12 +69,34 @@ class TrainPipeline:
             raise  SensorException(e,sys)
 
 
+
+    # Stage 5: Model Evaluation
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact):
+
+        try:
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
+
+        except  Exception as e:
+
+            raise  SensorException(e,sys)
+
+
+
+
+
     def run_pipeline(self):
         try:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
 
         except  Exception as e:
             raise  SensorException(e,sys)
