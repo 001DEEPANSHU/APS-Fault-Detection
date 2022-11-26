7438dbf5a05681498f9d7f441ae3c7913978a867
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-11-27 00:42:17 +0530

    doc update

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../source'))
 
 
 # -- Project information -----------------------------------------------------
@@ -40,6 +40,11 @@ extensions = [
 ]
 
 
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -81,3 +86,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+pygments_style = 'sphinx'
+todo_include_todos = False
